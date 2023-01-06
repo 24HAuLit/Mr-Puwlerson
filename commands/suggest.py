a0f2c596058e47a931b8a3cbc588f6505e220bf9
24h <24haulit@protonmail.com>
2023-01-06 21:02:00 +0100

    Added cooldown

--- commands/suggest.py
+++ commands/suggest.py
@@ -1,6 +1,8 @@
 import asyncio
+import sqlite3
 import interactions
 from datetime import datetime
+from interactions.ext.enhanced import cooldown
 from listeners.suggestion.components.accept import suggest_accept
 from listeners.suggestion.components.deny import suggest_deny
 
@@ -11,31 +13,45 @@ class Suggestion(interactions.Extension):
 
     counter = 0
 
+    async def cooldown_err(self, ctx, amount):
+        return await ctx.send(f"Tu dois encore attendre {amount} minutes avant de pouvoir réutiliser cette commande.",
+                              ephemeral=True)
+
     @interactions.extension_command()
+    @cooldown(minutes=10, error=cooldown_err, type="user")
     @interactions.option("Ecris ta suggestion ici.")
     async def suggest(self, ctx: interactions.CommandContext, suggestion: str):
         """Pour pouvoir proposer une suggestion."""
 
-        channel = await interactions.get(self.bot, interactions.Channel, object_id=1011704888679477369)
-        self.counter += 1
-        em = interactions.Embed(
-            title="Nouvelle suggestion #%d" % self.counter,
-            description=suggestion,
-            color=0xFFF000,
-            timestamp=datetime.utcnow()
-        )
-        em.set_footer(
-            icon_url=ctx.member.user.avatar_url,
-            text=f"Suggestion proposé par {ctx.author}."
-        )
+        conn = sqlite3.connect('./Database/puwlerson.db')
+        c = conn.cursor()
+        c.execute(f'SELECT * from blacklist WHERE user_id = {int(ctx.author.id)}')
+        row = c.fetchone()
 
-        await ctx.send(f"Votre suggestion a bien été posté dans {channel.mention}", ephemeral=True)
-        message = await channel.send(embeds=em, components=[suggest_accept(), suggest_deny()])
-        await message.create_reaction("⬆️")
-        await asyncio.sleep(1)
-        await message.create_reaction("⬇️")
+        if row is not None:
+            await ctx.send("Désolé, mais vous êtes blacklist. Vous ne pouvez donc pas envoyé de suggestion.", ephemeral=True)
+        else:
+            channel = await interactions.get(self.bot, interactions.Channel, object_id=1011704888679477369)
+            self.counter += 1
+            em = interactions.Embed(
+                title="Nouvelle suggestion #%d" % self.counter,
+                description=suggestion,
+                color=0xFFF000,
+                timestamp=datetime.utcnow()
+            )
+            em.set_footer(
+                icon_url=ctx.member.user.avatar_url,
+                text=f"Suggestion proposé par {ctx.author.username}#{ctx.author.discriminator}."
+            )
+
+            await ctx.send(f"Votre suggestion a bien été posté dans {channel.mention}", ephemeral=True)
+            message = await channel.send(embeds=em, components=[suggest_accept(), suggest_deny()])
+            await message.create_reaction("⬆️")
+            await asyncio.sleep(1)
+            await message.create_reaction("⬇️")
+
+        conn.close()
 
 
 def setup(bot):
     Suggestion(bot)
-    
