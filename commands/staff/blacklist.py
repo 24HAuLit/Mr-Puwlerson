bfed6eca63a8ea60b0119367ec40035e084b049f
24h <24haulit@protonmail.com>
2023-01-06 21:01:48 +0100

    Added DM

--- commands/staff/blacklist.py
+++ commands/staff/blacklist.py
@@ -12,9 +12,12 @@ class Blacklist(interactions.Extension):
     @interactions.option("Membre à blacklist")
     @interactions.option("Raison du blacklist")
     async def blacklist(self, ctx: interactions.CommandContext, user: interactions.User, reason: str):
+        """Pour pouvoir blacklist un membre des reports et des suggestions."""
+
         if DATA["roles"]["Admin"] in ctx.author.roles or DATA["roles"]["Owner"] in ctx.author.roles:
             user_id = user.id
             channel = await interactions.get(self.bot, interactions.Channel, object_id=1060273879018381405)
+            dm = user_id
 
             conn = sqlite3.connect('./Database/puwlerson.db')
             c = conn.cursor()
@@ -31,6 +34,16 @@ class Blacklist(interactions.Extension):
             em.set_footer(text=f"Staff ID : {ctx.author.id} | User ID : {user_id}")
 
             await channel.send(embeds=em)
+
+            em_dm = interactions.Embed(
+                title="🔒・Blacklist",
+                description=f"Vous avez été blacklist par **{ctx.author.username}#{ctx.author.discriminator}** pour **{reason}**.\nVous pourrez être unblacklist si vous êtes gentil ou après un certain temps.",
+                color=0xFF0000,
+                timestamp=datetime.utcnow()
+            )
+            em_dm.set_footer(icon_url=ctx.author.get_avatar_url(), text=f"Staff : {ctx.author.username}#{ctx.author.discriminator} ({ctx.author.id})")
+            await user.send(embeds=em_dm)
+
         else:
             await ctx.send(":x: Vous n'avez pas la permission d'utiliser cette commande'.", ephemeral=True)
             interactions.StopCommand()
--- commands/staff/unblacklist.py
+++ commands/staff/unblacklist.py
@@ -11,7 +11,9 @@ class UnBlacklist(interactions.Extension):
     @interactions.extension_command()
     @interactions.option("Membre à unblacklist")
     @interactions.option("Raison du unblacklist", required=False)
-    async def unblacklist(self, ctx: interactions.CommandContext, user: interactions.User, reason: str = None):
+    async def unblacklist(self, ctx: interactions.CommandContext, user: interactions.User, reason: str = "Aucune raison"):
+        """Pour pouvoir unblacklist un membre."""
+
         if DATA["roles"]["Admin"] in ctx.author.roles or DATA["roles"]["Owner"] in ctx.author.roles:
             user_id = user.id
             reason = reason
@@ -32,6 +34,17 @@ class UnBlacklist(interactions.Extension):
             em.set_footer(text=f"Staff ID : {ctx.author.id} | User ID : {user.id}")
 
             await channel.send(embeds=em)
+
+            em_dm = interactions.Embed(
+                title="🔓・Unblacklist",
+                description=f"Vous avez été unblacklist par **{ctx.author.username}#{ctx.author.discriminator}** pour **{reason}**.\nVous avez été gentil, c'est bien, maintenant continuer sur cette voie.",
+                color=0x00FF00,
+                timestamp=datetime.utcnow()
+            )
+            em_dm.set_footer(icon_url=ctx.author.get_avatar_url(),
+                             text=f"Staff : {ctx.author.username}#{ctx.author.discriminator} ({ctx.author.id})")
+            await user.send(embeds=em_dm)
+
         else:
             await ctx.send(":x: Vous n'avez pas la permission d'utiliser cette commande'.", ephemeral=True)
             interactions.StopCommand()
