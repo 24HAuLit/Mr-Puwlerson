0230044b12d204c129200058c68b71c84db6eb4c
24h <24haulit@protonmail.com>
2023-01-09 22:44:15 +0100

    Deleted files

--- src/listeners/logs/channel.py
+++
@@ -1,9 +0,0 @@
-# import interactions
-#
-#
-# class Channel(interactions.Extension):
-#     def __init__(self, bot):
-#         self.bot: interactions.Client = bot
-#
-#     # @interactions.extension_listener("on_channel_create")
-#     # async def channel_create(self, ):
