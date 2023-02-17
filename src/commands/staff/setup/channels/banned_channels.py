c93b2e6c76e6d5e30479964a285bdc7296bf9ffd
24h <24haulit@protonmail.com>
2023-02-17 20:41:58 +0100

    Edited banned_channels.py

--- src/commands/staff/setup/channels/banned_channels.py
+++ src/commands/staff/setup/channels/banned_channels.py
@@ -7,25 +7,25 @@ class BannedChannels(interactions.Extension):
         self.bot: interactions.Client = bot
 
     @interactions.extension_component("banned_channels")
-    async def select(self, ctx: interactions.ComponentContext, choice: list[str]):
+    async def select(self, ctx: interactions.ComponentContext, choice: list[int]):
         guild = await ctx.get_guild()
         conn = sqlite3.connect(f"./Database/{guild.id}.db")
         c = conn.cursor()
 
         for i in range(len(choice)):
-            c.execute(f"""SELECT hidden from channels WHERE id = {int(choice[i])}""")
+            c.execute(f"""SELECT hidden from channels WHERE id = {choice[i]}""")
             row = c.fetchone()
             if row is None:
-                c.execute(f"""INSERT INTO channels VALUES (name ,{choice[i]}, NULL, 1)""")
-                channel = await interactions.get(self.bot, interactions.Channel, object_id=int(choice[i]))
+                c.execute(f"""INSERT INTO channels VALUES (name ,{choice[i]}, NULL, 1)""") # N'a aucun sens (faut que je la modifie)
+                channel = await interactions.get(self.bot, interactions.Channel, object_id=choice[i])
                 await ctx.send(f"**{channel.name}** est désormais un channel 'banni' des logs", ephemeral=True)
             elif row[0] == 1:
                 c.execute(f"""UPDATE channels SET hidden = 0 WHERE id = {choice[i]}""")
-                channel = await interactions.get(self.bot, interactions.Channel, object_id=int(choice[i]))
+                channel = await interactions.get(self.bot, interactions.Channel, object_id=choice[i])
                 await ctx.send(f"**{channel.name}** n'est plus un channel 'banni' des logs", ephemeral=True)
             else:
                 c.execute(f"""UPDATE channels SET hidden = 1 WHERE id = {choice[i]}""")
-                channel = await interactions.get(self.bot, interactions.Channel, object_id=int(choice[i]))
+                channel = await interactions.get(self.bot, interactions.Channel, object_id=choice[i])
                 await ctx.send(f"**{channel.name}** est désormais un channel 'banni' des logs", ephemeral=True)
 
         conn.commit()
