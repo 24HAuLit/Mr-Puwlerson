146f3563beba0bb5960c26a5b86f33f97e103c6f
24h <24haulit@protonmail.com>
2023-02-28 22:44:45 +0100

    Added update.py command : Initial Commit

---
+++ src/commands/staff/setup/update.py
@@ -0,0 +1,28 @@
+import os
+import sqlite3
+
+import interactions
+
+
+class Update(interactions.Extension):
+    def __init__(self, bot):
+        self.bot: interactions.Client = bot
+
+    @interactions.extension_command()
+    async def update(self, ctx: interactions.CommandContext):
+        """Mise à jour de la base de données du serveur si nécessaire."""
+        if ctx.author.id == ctx.guild.owner_id:
+            pass
+        else:
+            return await ctx.send("❌・Vous n'avez pas l'autorisation d'utiliser cette commande.")
+
+        if os.path.exists("./Database/{}.db".format(ctx.guild_id)) is False:
+            return await ctx.send("❌・La base de données n'a pas encore été configurée. Pour la configurer, `/setup "
+                                  "server`.")
+
+        await ctx.send("Commande en cours de développement...", ephemeral=True)
+        # await ctx.send("🔄・Mise à jour de la base de données en cours...")
+
+
+def setup(bot):
+    Update(bot)
