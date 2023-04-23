19d16d25ef0dbb5d3cda90a44e45c2f6cd24205d
24h <24haulit@protonmail.com>
2023-04-23 20:47:55 +0200

    Fixed error from message.py

--- src/listeners/logs/message.py
+++ src/listeners/logs/message.py
@@ -20,6 +20,9 @@ class Message(interactions.Extension):
         if os.path.exists(f'./Database/{guild.id}.db') is False:
             return
 
+        if message.author.bot:
+            return
+
         conn = sqlite3.connect(f'./Database/{guild.id}.db')
         c = conn.cursor()
         logs_create = await interactions.get(self.bot, interactions.Channel, object_id=
@@ -28,30 +31,27 @@ class Message(interactions.Extension):
 
         conn.close()
 
-        if message.author.bot:
+        if (is_hidden == 1) or (channel.type == interactions.ChannelType.DM):
             return
         else:
-            if (is_hidden == 1) or (channel.type == interactions.ChannelType.DM):
-                return
-            else:
-                em = interactions.Embed(
-                    title="🖊️・Nouveau message",
-                    url=message.url,
-                    description=f"**{message.author.username}#{message.author.discriminator}** vient d'envoyer un message sur **{guild.name}** ({guild.id}) dans le salon **{channel.name}** ({channel.id})",
-                    color=0x4CFF4C,
-                    timestamp=datetime.utcnow()
-                )
-                if message.content != '':
-                    em.add_field(name="**Message : **", value=content)
-                if message.attachments is not None:
-                    i: int = 0
-                    for _ in message.attachments:
-                        em.add_field(name=f"**Attachment {i + 1} (type : {message.attachments[i].content_type}): **",
-                                     value=message.attachments[i].url)
-                        i += 1
-                em.set_footer(text=f"Author ID : {message.author.id} | Message ID : {message.id}")
+            em = interactions.Embed(
+                title="🖊️・Nouveau message",
+                url=message.url,
+                description=f"**{message.author.username}#{message.author.discriminator}** vient d'envoyer un message sur **{guild.name}** ({guild.id}) dans le salon **{channel.name}** ({channel.id})",
+                color=0x4CFF4C,
+                timestamp=datetime.utcnow()
+            )
+            if message.content != '':
+                em.add_field(name="**Message : **", value=content)
+            if message.attachments is not None:
+                i: int = 0
+                for _ in message.attachments:
+                    em.add_field(name=f"**Attachment {i + 1} (type : {message.attachments[i].content_type}): **",
+                                 value=message.attachments[i].url)
+                    i += 1
+            em.set_footer(text=f"Author ID : {message.author.id} | Message ID : {message.id}")
 
-                await logs_create.send(embeds=em)
+            await logs_create.send(embeds=em)
 
     @interactions.extension_listener(name="on_message_update")
     async def edit_message(self, before: interactions.Message, after: interactions.Message):
@@ -65,6 +65,9 @@ class Message(interactions.Extension):
         if os.path.exists(f'./Database/{guild.id}.db') is False:
             return
 
+        if after.author.bot:
+            return
+
         conn = sqlite3.connect(f'./Database/{guild.id}.db')
         c = conn.cursor()
         logs_edit = await interactions.get(self.bot, interactions.Channel, object_id=
@@ -73,38 +76,35 @@ class Message(interactions.Extension):
 
         conn.close()
 
-        if after.author.bot:
+        if (is_hidden == 1) or (channel.type == interactions.ChannelType.DM):
             return
         else:
-            if (is_hidden == 1) or (channel.type == interactions.ChannelType.DM):
-                return
-            else:
-                em2 = interactions.Embed(
-                    title="📝・Modification de message",
-                    url=after.url,
-                    description=f"**{after.author.username}#{after.author.discriminator}** vient de modifier un message sur **{guild.name}** ({guild.id}) dans le salon **{channel.name}** ({channel.id})",
-                    color=0xFFFF00,
-                    timestamp=datetime.utcnow()
-                )
-                if after.content != '':
-                    if old_content == '':
-                        em2.add_field(name="**Ancien Message : **", value="Aucun message", inline=False)
-                    else:
-                        em2.add_field(name="**Ancien Message : **", value=old_content, inline=False)
-                    em2.add_field(name="**Nouveau Message : **", value=new_content, inline=False)
-                if before.attachments is not None:
-                    i: int = 0
-                    for j in before.attachments:
-                        em2.add_field(
-                            name=f"**Ancien Attachment {i + 1} (type : {before.attachments[i].content_type}): **",
-                            value=before.attachments[i].url)
-                        em2.add_field(
-                            name=f"**Nouveau Attachment {i + 1} (type : {after.attachments[i].content_type}): **",
-                            value=after.attachments[i].url)
-                        i += 1
-                em2.set_footer(text=f"Author ID : {after.author.id} | Message ID : {after.id}")
+            em2 = interactions.Embed(
+                title="📝・Modification de message",
+                url=after.url,
+                description=f"**{after.author.username}#{after.author.discriminator}** vient de modifier un message sur **{guild.name}** ({guild.id}) dans le salon **{channel.name}** ({channel.id})",
+                color=0xFFFF00,
+                timestamp=datetime.utcnow()
+            )
+            if after.content != '':
+                if old_content == '':
+                    em2.add_field(name="**Ancien Message : **", value="Aucun message", inline=False)
+                else:
+                    em2.add_field(name="**Ancien Message : **", value=old_content, inline=False)
+                em2.add_field(name="**Nouveau Message : **", value=new_content, inline=False)
+            if before.attachments is not None:
+                i: int = 0
+                for _ in before.attachments:
+                    em2.add_field(
+                        name=f"**Ancien Attachment {i + 1} (type : {before.attachments[i].content_type}): **",
+                        value=before.attachments[i].url)
+                    em2.add_field(
+                        name=f"**Nouveau Attachment {i + 1} (type : {after.attachments[i].content_type}): **",
+                        value=after.attachments[i].url)
+                    i += 1
+            em2.set_footer(text=f"Author ID : {after.author.id} | Message ID : {after.id}")
 
-                await logs_edit.send(embeds=em2)
+            await logs_edit.send(embeds=em2)
 
     @interactions.extension_listener(name="on_message_delete")
     async def message_delete(self, message: interactions.Message):
@@ -118,6 +118,9 @@ class Message(interactions.Extension):
         if os.path.exists(f'./Database/{guild.id}.db') is False:
             return
 
+        if message.author.bot:
+            return
+
         conn = sqlite3.connect(f'./Database/{guild.id}.db')
         c = conn.cursor()
 
@@ -128,8 +131,6 @@ class Message(interactions.Extension):
         conn.close()
 
         if message.author is None:
-            return
-        elif message.author.bot:
             return
         else:
             if (is_hidden == 1) or (channel.type == interactions.ChannelType.DM):
@@ -145,7 +146,7 @@ class Message(interactions.Extension):
                     em.add_field(name="**Message : **", value=content)
                 if message.attachments is not None:
                     i: int = 0
-                    for j in message.attachments:
+                    for _ in message.attachments:
                         em.add_field(name=f"**Attachment {i + 1} (type : {message.attachments[i].content_type}): **",
                                      value=message.attachments[i].url)
                         i += 1
